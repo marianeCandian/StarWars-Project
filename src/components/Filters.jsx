993650b33bb52074bc573b90040bedeadfffcf2e
Mariane Candian <mariane.candian@gmail.com>
2022-11-22 17:32:49 -0300

    Requisito 2 em andamento

---
+++ src/components/Filters.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useContext, useEffect } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+
+export default function Filters() {
+  const { setFilters } = useContext(StarWarsContext);
+  const [name, setName] = useState('');
+
+  const handleChange = ({ target }) => {
+    setName(target.value);
+  };
+
+  useEffect(() => {
+    setFilters(name);
+  }, [name, setFilters]);
+
+  return (
+    <form>
+      <label htmlFor="name">
+        <input
+          type="text"
+          name="name"
+          value={ name }
+          id="name"
+          onChange={ handleChange }
+          data-testid="name-filter"
+        />
+      </label>
+    </form>
+  );
+}
